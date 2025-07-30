3708b40f85aa0304c9f42ca4b6e345550e9719ee
Anatoliy Kotliar <blizzkon@gmail.com>
2025-07-30 09:57:17 +0300

    make human readable number fuction

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
-module.exports = function toReadable(/* number */) {
-  throw new Error('Not implemented');
+module.exports = function toReadable(number) {
+  const oneMass = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+  const teenMass = [
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+  const hundredMass = [
+    '',
+    '',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
+  const [hundreds, remainder] = [Math.floor(number / 100), number % 100];
+  const [ten, one] = [Math.floor(remainder / 10), remainder % 10];
+  const finalPart = [];
+  if (hundreds) finalPart.push(`${oneMass[hundreds]} hundred`);
+  if (remainder) {
+    if (remainder < 10) {
+      parts.push(oneMass[remainder]);
+    } else if (remainder < 20) {
+      parts.push(teenMass[remainder - 10]);
+    } else {
+      parts.push(hundredMass[ten] + (one ? ` ${oneMass[one]}` : ''));
+    }
+  }
+  if (!number) return 'zero';
+
+  return parts.join(' ');
 };
